28e32dcd4c48ed5c4c5c41c09f158b25b960bf88
mdominguezg <mdominguezg@106-06.info2>
2022-10-25 12:48:37 +0200

    commit

--- EjerciciosT3_MiguelDominguezGarrote/src/ejercicios/Ejercicio02.java
+++ EjerciciosT3_MiguelDominguezGarrote/src/ejercicios/Ejercicio02.java
@@ -1,10 +1,26 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio02 {
 
 	public static void main(String[] args) {
 		// bloque de variables
+		int num; 		// se guardara los numeros que el usuario quiera sumar 
+		int suma=0;			// se guarda la suma
 		
+		//creamos el Scanner y preguntamos por el numero 
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Introduzca un numero para sumar: ");		// pedimos al usuario que introduzca un numero
+		num=sc.nextInt();
+		while (num>=0) {												// mientras el numero introducido sea positivo el bucle contiunara
+			suma ++;													// cada vez que se introduzca un numero se sumara 1 a la variable suma
+			System.out.println("Introduzca un numero para sumar: ");	// volvemos a preguntar
+			num=sc.nextInt();
+		}
+		System.out.println("En total hay: " + suma + " numeros");	// mostramos el resultado
+		// cerramos el scanner
+		sc.close();
 	}
 
 }
--- EjerciciosT3_MiguelDominguezGarrote/src/ejercicios/Ejercicio03.java
+++ EjerciciosT3_MiguelDominguezGarrote/src/ejercicios/Ejercicio03.java
@@ -2,23 +2,25 @@ package ejercicios;
 
 import java.util.Scanner;
 
-public class Ejercicio01 {
+public class Ejercicio03 {
 
 	public static void main(String[] args) {
 		// bloque de variables
-		int num; 		// se guardara los numeros que el usuario quiera sumar 
-		int suma=0;			// se guarda la suma
+		int num; 				// se guardara los numeros que el usuario quiera sumar 
+		int suma=0;				// se guarda la suma
+		int conteo=0;			// se guarda la  cuenta de los numeros
 		
 		//creamos el Scanner y preguntamos por el numero 
 		Scanner sc= new Scanner(System.in);
 		System.out.println("Introduzca un numero para sumar: ");		// pedimos al usuario que introduzca un numero
 		num=sc.nextInt();
 		while (num>=0) {												// mientras el numero introducido sea positivo el bucle contiunara
-			suma +=num;													// sumamos el numero de la variable num
+			conteo ++;													// cada vez que se introduzca un numero se sumara 1 a la variable suma
+			suma +=num;												 	// se va sumando los numeros 
 			System.out.println("Introduzca un numero para sumar: ");	// volvemos a preguntar
 			num=sc.nextInt();
 		}
-		System.out.println("El resultado de la suma es: "+suma);	// mostramos el resultado
+		System.out.println("La media de los numeros es: "+ suma/conteo);	// mostramos el resultado
 		// cerramos el scanner
 		sc.close();
 	}
