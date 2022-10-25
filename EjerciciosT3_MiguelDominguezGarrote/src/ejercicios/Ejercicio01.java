072467b59c19b6ad6e8c484c1d38ce5bb0d4f97a
mdominguezg <mdominguezg@106-06.info2>
2022-10-25 12:26:22 +0200

    commit

--- EjerciciosT3_MiguelDominguezGarrote/src/ejercicios/Ejercicio01.java
+++ EjerciciosT3_MiguelDominguezGarrote/src/ejercicios/Ejercicio01.java
@@ -1,10 +1,25 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio01 {
 
 	public static void main(String[] args) {
 		// bloque de variables
-
+		int num; 		// se guardara los numeros que el usuario quiera sumar 
+		int suma=0;			// se guarda la sum
+		
+		//creamos el Scanner y preguntamos por el numero 
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Introduzca un numero para sumar: ");
+		num=sc.nextInt();
+		while (num>=0) {
+			suma +=num;
+			System.out.println("El resultado de la suma es: "+suma);
+			System.out.println("Introduzca un numero para sumar: ");
+			num=sc.nextInt();
+		}
+	sc.close();
 	}
 
 }
